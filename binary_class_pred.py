c038697d61f069b83b2b8af23319ab5b4855e154
Abhik Seal <abhik1368@gmail.com>
2023-09-24 21:35:45 -0500

    binary class pred for windows
    binary class pred for windows code

--- binary_class_pred.py
+++ binary_class_pred.py
@@ -14,13 +14,6 @@ from transformers import Trainer
 from prepare_finetuning_data import smiles_to_selfies
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="bbbp", help="task selection.")
-parser.add_argument("--tokenizer_name", default="data/RobertaFastTokenizer", metavar="/path/to/dataset/", help="Tokenizer selection.")
-parser.add_argument("--pred_set", default="data/finetuning_datasets/classification/bbbp/bbbp_mock.csv", metavar="/path/to/dataset/", help="Test set for predictions.")
-parser.add_argument("--training_args", default= "data/finetuned_models/modelO_bbbp_scaffold_optimized/training_args.bin", metavar="/path/to/dataset/", help="Trained model arguments.")
-parser.add_argument("--model_name", default="data/finetuned_models/modelO_bbbp_scaffold_optimized",  metavar="/path/to/dataset/", help="Path to the model.")
-args = parser.parse_args()
 
 class SELFIESTransformers_For_Classification(BertPreTrainedModel):
     def __init__(self, config):
@@ -42,13 +35,6 @@ class SELFIESTransformers_For_Classification(BertPreTrainedModel):
             outputs = (loss,) + outputs
         return outputs  # (loss), logits, (hidden_states), (attentions)
 
-model_class = SELFIESTransformers_For_Classification
-config_class = RobertaConfig
-tokenizer_name = args.tokenizer_name
-
-tokenizer_class = RobertaTokenizerFast
-tokenizer = tokenizer_class.from_pretrained(tokenizer_name, do_lower_case=False)
-
 # Prepare and Get Data
 class SELFIESTransfomers_Dataset(Dataset):
     def __init__(self, data, tokenizer, MAX_LEN):
@@ -62,29 +48,51 @@ class SELFIESTransfomers_Dataset(Dataset):
     def __getitem__(self, index):
         item = {key: self.examples[key][index] for key in self.examples}
       
-        return item
-    
-pred_set = pd.read_csv(args.pred_set)
-pred_df_selfies = smiles_to_selfies(pred_set)
+        return item 
 
-MAX_LEN = 128
+if __name__ == '__main__':
 
-pred_examples = (pred_df_selfies.iloc[:, 0].astype(str).tolist())
-pred_dataset = SELFIESTransfomers_Dataset(pred_examples, tokenizer, MAX_LEN)
+    # Main execution logic goes here
 
-training_args = torch.load(args.training_args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", default="bbbp", help="task selection.")
+    parser.add_argument("--tokenizer_name", default="data/RobertaFastTokenizer", metavar="/path/to/dataset/", help="Tokenizer selection.")
+    parser.add_argument("--pred_set", default="data/finetuning_datasets/classification/bbbp/bbbp_mock.csv", metavar="/path/to/dataset/", help="Test set for predictions.")
+    parser.add_argument("--training_args", default= "data/finetuned_models/modelO_bbbp_scaffold_optimized/training_args.bin", metavar="/path/to/dataset/", help="Trained model arguments.")
+    parser.add_argument("--model_name", default="data/finetuned_models/modelO_bbbp_scaffold_optimized",  metavar="/path/to/dataset/", help="Path to the model.")
+    args = parser.parse_args()
 
-model_name = args.model_name
-config = config_class.from_pretrained(model_name, num_labels=2)
-bbbp_model = model_class.from_pretrained(model_name, config=config)
 
-trainer = Trainer(model=bbbp_model, args=training_args)  # the instantiated 🤗 Transformers model to be trained  # training arguments, defined above  # training dataset  # evaluation dataset
-raw_pred, label_ids, metrics = trainer.predict(pred_dataset)
-print(raw_pred)
-y_pred = np.argmax(raw_pred, axis=1).astype(int)
-res = pd.concat([pred_df_selfies, pd.DataFrame(y_pred, columns=["prediction"])], axis = 1)
+    model_class = SELFIESTransformers_For_Classification
+    config_class = RobertaConfig
+    tokenizer_name = args.tokenizer_name
 
-if not os.path.exists("data/predictions"):
-    os.makedirs("data/predictions")
+    tokenizer_class = RobertaTokenizerFast
+    tokenizer = tokenizer_class.from_pretrained(tokenizer_name, do_lower_case=False)
 
-res.to_csv("data/predictions/{}_predictions.csv".format(args.task), index=False)
+
+        
+    pred_set = pd.read_csv(args.pred_set)
+    pred_df_selfies = smiles_to_selfies(pred_set)
+
+    MAX_LEN = 128
+
+    pred_examples = (pred_df_selfies.iloc[:, 0].astype(str).tolist())
+    pred_dataset = SELFIESTransfomers_Dataset(pred_examples, tokenizer, MAX_LEN)
+
+    training_args = torch.load(args.training_args)
+
+    model_name = args.model_name
+    config = config_class.from_pretrained(model_name, num_labels=2)
+    bbbp_model = model_class.from_pretrained(model_name, config=config)
+
+    trainer = Trainer(model=bbbp_model, args=training_args)  # the instantiated 🤗 Transformers model to be trained  # training arguments, defined above  # training dataset  # evaluation dataset
+    raw_pred, label_ids, metrics = trainer.predict(pred_dataset)
+    print(raw_pred)
+    y_pred = np.argmax(raw_pred, axis=1).astype(int)
+    res = pd.concat([pred_df_selfies, pd.DataFrame(y_pred, columns=["prediction"])], axis = 1)
+
+    if not os.path.exists("data/predictions"):
+        os.makedirs("data/predictions")
+
+    res.to_csv("data/predictions/{}_predictions.csv".format(args.task), index=False)
